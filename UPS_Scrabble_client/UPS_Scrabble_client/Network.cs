8ff42b11e4efabe17a74ff0585f804d8cd1dd94e
pivovard <pivovar.david@gmail.com>
2017-01-07 18:06:59 +0100

    comment using System.Threading.Tasks; kvuli monu

--- UPS_Scrabble_client/UPS_Scrabble_client/Network.cs
+++ UPS_Scrabble_client/UPS_Scrabble_client/Network.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace UPS_Scrabble_client
